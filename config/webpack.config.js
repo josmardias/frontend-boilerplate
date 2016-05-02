1a8ccd0dfcc2a3319b1ec0e78132fcabb9c3ea05
Josmar Dias <josmarnet@gmail.com>
2016-05-02 01:38:03 -0300

    use development server from webpack-dev-server
    It uses webpack to create in memory build. Still needs to properly
    setup hot module replacement. Right now Live Reload is working fine
    (every code update seems to fail on HMR, tiggering a full page reload)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,30 @@
+const path = require('path')
+
+const PATHS = {
+  app: path.join(__dirname, '..', 'src', 'main.js'),
+  www: path.join(__dirname, '..', 'www'),
+  build: path.join(__dirname, '..', 'www', 'js'),
+}
+
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    app: [
+      PATHS.app,
+    ],
+  },
   output: {
-    path: 'www/js',
+    path: PATHS.build,
+    publicPath: '/assets/',
     filename: 'app.js',
   },
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
+  },
+  devServer: {
+    contentBase: PATHS.www,
+    noInfo: true,
+    progress: true,
   },
 }
