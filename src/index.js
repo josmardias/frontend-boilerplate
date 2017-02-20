2ce6ccd65e2be3ba09fb07576f05fc51dc9c208e
Josmar Dias <josmarnet@gmail.com>
2017-02-20 19:59:39 -0300

    refactor using Create React App

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './app'
+import App from './App'
+import './index.css'
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
+  <App />,
   rootElement
 )
 
 if (module.hot) {
-  module.hot.accept('./app', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./app').default // eslint-disable-line
+  module.hot.accept('./App', () => {
+    // If using Webpack 2 with ES modules mode, you can
+    //  use <App /> here rather than require() a <NextApp />.
+    const NextApp = require('./App').default
     ReactDOM.render(
-      <AppContainer>
-        <NextApp place="world" />
-      </AppContainer>,
+      <NextApp />,
       rootElement
     )
   })
